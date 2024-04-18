71fa67388a372c694e8e56d5af42d47082958ded
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-18 18:03:14 +0900

    기본 블로그 프로젝트 세팅

---
+++ src/components/molecules/Input/index.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from '.';
+
+const meta = {
+  title: 'Molecules/Input',
+  component: Input,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'Title',
+    value: '블로그 제목',
+    onChange: (text: string) => { /* onChange 이벤트 핸들러 구현 */ },
+  },
+};
